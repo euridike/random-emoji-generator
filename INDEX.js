f650a8f4c2d914f0fe2f568b6b8e22c4fcb27b03
“euridike” <reka.szuecs@icloud.com>
2022-05-08 20:05:57 +0300

    Add first working version

--- INDEX.js
+++ INDEX.js
@@ -1,1 +1,18 @@
-console.log(`string of tex`);
+import emoji from 'node-emoji';
+
+if (process.argv.length < 3) {
+  console.log(emoji.random().emoji);
+} else if (!emoji.hasEmoji(process.argv[2])) {
+  console.log('Invalid emoji');
+} else {
+  console.log(emoji.get(process.argv[2]));
+}
+
+//import emoji from 'node-emoji';
+
+//if (process.argv.length > 2 && emoji.hasEmoji(process.argv[2])) {
+//  console.log(emoji.get(process.argv[2]));
+//} else if (process.argv.length > 2 && !emoji.hasEmoji(process.argv[2])) {
+//  console.log('Invalid emoji');
+//} else {
+//  console.log(emoji.random().emoji)}
